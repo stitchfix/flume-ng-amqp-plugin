c92af1f10eb3596af4126df8b536acf65dddc3bc
Ewan Oglethorpe <eoglethorpe@stitchfix.com>
2015-02-11 00:53:45 -0800

    "added routingkey decomposition and sanity checks"

--- src/main/java/org/apache/flume/amqp/SFEventBatchDeliveryListener.java
+++ src/main/java/org/apache/flume/amqp/SFEventBatchDeliveryListener.java
@@ -2,6 +2,7 @@ package org.apache.flume.amqp;
 
 import java.util.Map;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.flume.Clock;
 import org.apache.flume.Event;
 import org.apache.flume.Source;
@@ -26,11 +27,62 @@ public class SFEventBatchDeliveryListener extends EventBatchDeliveryListener {
 
 	@Override
 	public Event createEventFrom(Delivery delivery) {
-		// TODO Auto-generated method stub
+		/** add additional metadata to event**/
 		Event event = super.createEventFrom(delivery);
-		Map<String, String> headers = event.getHeaders();
-		headers.put("foo", "bar");
-		event.setHeaders(headers);
+
+		String actionHeader = null;
+		String actionIDHeader = null;
+		String lowerDomainHeader = null;
+		String topDomainHeader = null;
+		String modelHeader = null;
+		
+		//will be set to true if routing key is malformed
+		boolean malformed = false;
+		
+		//get headers of current log
+		Map<String, String> currentHeader = event.getHeaders();
+		
+		//pull the routingKey from the current log
+		String currentRoutingKey = currentHeader.get("routingKey");
+		
+		//split the routingKey into individual parts
+		//RK format: topDomain.lowerDomain.model(cant be multiple .s).actionId.action
+		//sample: sf.merch.style.123.created
+		String [] RKparts = currentRoutingKey.split("\\.");
+		
+		//if this is a valid routing key based on number of segments
+		if(RKparts.length >= 5)
+		{
+			actionHeader = RKparts[RKparts.length-1];
+			actionIDHeader = RKparts[RKparts.length-2];
+			lowerDomainHeader = RKparts[1];
+			topDomainHeader = RKparts[0];
+			
+			//since the model can span multiple segments, we must do this logic and concat
+			//the parts with a "-" to avoid using "." with DB tables
+			modelHeader="";
+			for(int i = 2; i < RKparts.length - 2; i++)
+			{
+				//only do if > 2 as this means we will have a new part of model to append 
+				if(i>2)
+					modelHeader+="-";
+				modelHeader+=RKparts[i];
+			}
+		}
+		//add new headers 
+		currentHeader.put("action", actionHeader);
+		currentHeader.put("actionID", actionIDHeader);
+		currentHeader.put("lowerDomain", lowerDomainHeader);
+		currentHeader.put("topDomain", topDomainHeader);
+		currentHeader.put("model", modelHeader);
+		currentHeader.put("malformed", malformed+"");
+		
+		//check to see if timestamp is valid, and if not set it to current time
+		if(!StringUtils.isNumeric(currentHeader.get("timestamp")));
+			currentHeader.put("timestamp",  Long.toString(System.currentTimeMillis()/1000));
+		
+		//add new headers (in combinatino with previous ones) to current event
+		event.setHeaders(currentHeader);
 		return event;
 	}
 	
